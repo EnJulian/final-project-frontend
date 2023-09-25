e852ec843f95fe5f7bcef86225c30f0f83b6faf5
niiodoi15 <134379645+niiodoi15@users.noreply.github.com>
2023-09-25 23:32:33 +0000

    registration form

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,233 +1,179 @@
 <script setup>
+import { ref } from 'vue';
+import { RouterLink } from "vue-router";
+
+// Define a reactive property to track the password visibility
+const passwordVisible = ref(false);
+
+// Function to toggle password visibility
+function togglePassword() {
+  passwordVisible.value = !passwordVisible.value;
+}
 
 </script>
 
 <template>
-<div class="User">
-    <div class="maincontainer">
-      <div class="Enyatalogo">
-        <img src="/src/assets/icons/logo.svg" alt="Enyata Logo" srcset="" />
+  <section>
+    <div class="main">
+      <div class="title">
+        <img class="logo" src="../assets/icons/small_logo.svg" />
+        <h3>Application Form</h3>
       </div>
-      <div class="Applicants signup">
-        <h1>Applicant Sign Up</h1>
-      </div>
-      <div class="ErrorServer" v-show="error">{{ error }} &#9888;</div>
-      <div class="Applicantform">
-        <form @submit.prevent="createUser">
-          <div class="inputs">
-            <div>
-              <label>First Name</label>
-              <input
-                v-model.trim="user.firstName"
-                @keydown="clearError('fname')"/>
-              <p v-show="fnameError">{{ fnameError }}</p>
-            </div>
-            <div>
-              <label>Last Name</label>
-              <input
-                v-model.trim="user.lastName"
-                @keydown="clearError('lname')" />
-              <p>{{ lastError }}</p>
-            </div>
-            <div>
-              <label>Email Address</label>
-              <input
-                v-model.trim="user.emailAddress"
-                @keydown="clearError('email')"/>
-              <p>{{ emailError }}</p>
-            </div>
-            <div>   
-              <label>Phone Number</label>
-              <input
-                v-model.trim="user.phoneNumber"
-                type="tel"
-                @keydown="clearError('phone')"/>
-              <p>{{ numberError }}</p>
-            </div>
-            <div class="password-wrap">
-              <label>Password</label>
-              <input
-                :type="inputTypeIcon"
-                v-model.trim="user.password"
-                @keydown="clearError('password')"
-              />
-              <p>{{ passwordError }}</p>
-              <div class="icon" @click.prevent="toggleInputIcon">
-                <span v-if="inputTypeIcon == 'password'"><div class="eyelogo">
-                    <img
-                      src="/src/assets/icons/Eye.png"
-                      alt="eye icon" srcset=""/>
-                  </div>
-                </span>
-                <span v-else ><div class="eyelogo">
-                    <img
-                      src="/src/assets/icons/Eye.png"
-                      alt="eye icon" srcset=""/></div></span>
-              </div>
-              <p>{{ passwordError }}</p>
-            </div>
-
-            <div class="password-confirm">
-              <label>Confirm Password</label>
-              <input
-                :type="inputTypeIcon"
-                v-model="user.confirmPassword"
-                @keydown="clearError('confirmPassword')" />
-              <p>{{ confirmError }}</p>
-              <div class="icon" @click.prevent="toggleInputIcon">
-                <span v-if="inputTypeIcon == 'password'"
-                  ><div class="eyelogo1">
-                    <img
-                      src="/src/assets/icons/Eye.png"
-                      alt="eye icon"
-                      srcset="" />
-                  </div>
-                </span>
-                <span v-else><div class="eyelogo1">
-                    <img
-                      src="/src/assets/icons/Eye.png"
-                      alt="eye icon"
-                      srcset=""/></div></span>
-              </div>
+      <div class="forms">
+        <div class="forms-layout">
+          <div class="input-options">
+            <label for="input">First Name</label>
+            <input type="text" class="field-input">
+          </div>
+          <div class="input-options">
+            <label for="input">Last Name</label>
+            <input type="text" class="field-input">
+          </div>
+        </div>
+        <div class="forms-layout">
+          <div class="input-options">
+            <label for="input">Email Address</label>
+            <input type="text" class="field-input">
+          </div>
+          <div class="input-options">
+            <label for="input">Phone Number</label>
+            <input type="text" class="field-input">
+          </div>
+        </div>
+        <div class="forms-layout">
+          <div class="input-options">
+            <label for="password">Password</label>
+            <div class="password-field">
+              <input :type="passwordVisible ? 'text' : 'password'" class="field-input">
+              <span class="password-toggle" @click="togglePassword">
+                <img src="../assets/icons/eye.svg" />
+              </span>
             </div>
           </div>
-          <button type="submit">Sign Up</button>
-        </form>
-        <h2>
-          Already have an account?<router-link to="/login" ><span> Sign In</span></router-link>
-        </h2>
+          <div class="input-options">
+            <label for="confirmPassword">Confirm Password</label>
+            <div class="password-field">
+              <input :type="passwordVisible ? 'text' : 'password'" class="field-input">
+              <span class="password-toggle" @click="togglePassword">
+                <img src="../assets/icons/eye.svg" />
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="btn">
+          <RouterLink to="/login"><button>Sign Up</button></RouterLink>
+          <p>Already have an account? <RouterLink to="/login" class="link">Sign In</RouterLink>
+          </p>
+        </div>
+
       </div>
     </div>
-  </div>
+  </section>
 </template>
 
 <style scoped>
-img {
-  margin-top: 100px;
-  margin-bottom: 26px;
-}
-button {
-  font-family: "Lato";
-  font-size: 16px;
-  line-height: 19px;
-  height: 50px;
-  width: 520px;
-  border: none;
-  margin-top: 40px;
-  background: #7557d3;
-  border-radius: 4px;
-  color: #ffffff;
-  text-align: center;
-  cursor: pointer;
-  font-weight: 700;
-}
-p {
-  color: red;
-  font-size: 12px;
-  text-align: start;
-  margin-top: 5px;
-}
-h1 {
-  font-family: "Lato";
-  font-style: italic;
-  font-weight: 500;
-  font-size: 24px;
-  line-height: 29px;
-  margin-bottom: 70px;
-  text-align: center;
-  color: #2b3c4e;
-}
-.Enyatalogo {
-  text-align: center;
-}
-input {
-  width: 379px;
-  height: 48px;
-  left: 767px;
-  top: 394px;
-  margin-top: 3px;
-  border: 1.5px solid #bdbdbd;
-  border-radius: 4px;
-  padding-left: 10px;
-}
-label {
-  font-family: "Lato";
-  font-style: normal;
-  font-weight: 400;
-  font-size: 14px;
-  line-height: 17px;
-  margin-top: 20px;
-  color: #4f4f4f;
-}
-.User {
-  height: 100vh;
+.title {
   display: flex;
   justify-content: center;
+  flex-direction: column;
   align-items: center;
+  padding-top: 160px;
+  gap: 24px;
 }
-.maincontainer {
+
+.title h3 {
+  color: #2B3C4E;
+  font-family: 'Lato';
+  font-size: 24px;
+  font-style: italic;
+  font-weight: 500;
+  line-height: normal;
+}
+
+.forms {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-top: 68px;
+  gap: 27px;
+}
+
+.forms-layout {
+  display: flex;
+  gap: 62px;
+}
+
+.input-options {
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+}
+
+.input-options label {
+  color: #4F4F4F;
+  font-family: 'Lato';
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+}
+
+.field-input {
+  border-radius: 4px;
+  border: 1.5px solid #BDBDBD;
+  width: 379px;
+  height: 48px;
+}
+
+.btn {
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  padding-top: 13px;
+  gap: 10px;
 }
 
-.Applicantform {
-  gap: 62px;
-  justify-content: center;
+button {
+  color: #FFF;
+  font-family: 'Lato';
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  padding: 16px 233px;
+  background: #7557D3;
+  border: none;
 }
 
-.eyelogo {
-  position: absolute;
-  bottom: -8px;
-  right: 14px;
-  cursor: pointer;
-}
-.password-wrap {
-  position: relative;
-}
-
-.password-confirm {
-  position: relative;
-}
-.eyelogo1 {
-  top: -40px;
-  left: 350px;
-  position: absolute;
-  cursor: pointer;
-}
-
-label {
-  display: block;
-  text-align: start;
-}
-h2 {
-  font-family: "Lato";
+.btn p {
+  color: #4F4F4F;
+  font-family: 'Lato';
+  font-size: 14px;
   font-style: italic;
   font-weight: 400;
-  font-size: 14px;
-  line-height: 17px;
-  color: #4f4f4f;
-  margin-top: 12px;
-  text-align: center;
+  line-height: normal;
 }
-form {
-  text-align: center;
+
+.link a {
+  color: #1A2C56;
 }
-.inputs {
-  display: grid;
-  grid-template-columns: max-content max-content;
-  gap: 22px 62px;
-  justify-content: center;
+
+/* / Additional CSS for the password toggle icon /  */
+.password-field {
+  position: relative;
 }
-span {
-  color: #1a2c56;
+
+.password-toggle {
+  position: absolute;
+  top: 50%;
+  right: 10px;
+  transform: translateY(-50%);
+  cursor: pointer;
+  color: #000;
 }
-.ErrorServer {
-  width: max-content;
-  border-radius: 5px;
-  background: #d68f8a;
-  color: #fff;
-  padding: 10px;
-}
-</style>
+
+/* / Style the eye icon /  */
+.password-toggle img {
+  width: 18px;
+}</style>

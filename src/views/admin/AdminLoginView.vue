b8a7f25bb2e5b8cf204a270222f23df6c5fbc943
niiodoi15 <134379645+niiodoi15@users.noreply.github.com>
2023-09-28 21:58:33 +0000

    admin login page ui

--- src/views/admin/AdminLoginView.vue
+++ src/views/admin/AdminLoginView.vue
@@ -1,5 +1,152 @@
-<script setup></script>
+<script setup>
+    import { ref } from 'vue';
+    import { RouterLink } from "vue-router";
 
-<template></template>
+// Define a reactive property to track the password visibility
+const passwordVisible = ref(false);
 
-<style scoped></style>
+// Function to toggle password visibility
+function togglePassword() {
+    passwordVisible.value = !passwordVisible.value;
+}
+
+</script>
+
+<template>
+    <div class="content">
+        <div class="container">
+            <div class="main">
+                <img src="../assets/icons/small_logo.svg">
+                <p>Admin Log In</p>
+            </div>
+
+            <div class="forms">
+                <div class="input-options">
+                    <label for="input">Email Address</label>
+                    <input type="text" class="field-input">
+                </div>
+                <div class="input-options">
+                    <label for="password">Password</label>
+                    <div class="password-field">
+                        <input :type="passwordVisible ? 'text' : 'password'" class="field-input">
+                        <span class="password-toggle" @click="togglePassword">
+                            <img src="../../assets/icons/eye.svg" alt="show password"/>
+                        </span>
+                    </div>
+                </div>
+                <div class="btn">
+                    <RouterLink to="/adminDashboard"><button>Sign In</button></RouterLink>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .content{
+        height: 100vh;
+        background-color: #7557D3;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .container{
+        width: 100%;
+        height: 624px;
+        background-image: url('../../assets/icons/bglogin.svg');
+        background-repeat: no-repeat;
+        background-position: right;
+        background-size: contain;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+    .main {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    /* padding-top: 225px; */
+    gap: 24px;
+    padding-bottom: 69px;
+}
+
+.main p {
+    color: #FFF;
+    font-family: Lato;
+    font-size: 24px;
+    font-style: italic;
+    font-weight: 500;
+    line-height: normal;
+}
+.forms {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 22px;
+}
+
+.input-options {
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+}
+
+.input-options label {
+    color: #fff;
+    font-family: 'Lato';
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+}
+
+.field-input {
+    border-radius: 4px;
+    border: 1.5px solid #BDBDBD;
+    width: 379px;
+    height: 48px;
+    background-color: transparent;
+}
+
+/* / Additional CSS for the password toggle icon /  */
+.password-field {
+    position: relative;
+}
+
+.password-toggle {
+    position: absolute;
+    top: 50%;
+    right: 10px;
+    transform: translateY(-50%);
+    cursor: pointer;
+    color: #fff;
+}
+
+/* / Style the eye icon /  */
+.password-toggle img {
+    width: 18px;
+}
+
+.btn {
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+}
+
+button {
+    padding: 16px 165px 14px 165px;
+    color: #7557D3;
+    font-family: 'Lato';
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    background: #fff;
+    border: none;
+    cursor: pointer;
+}
+
+
+</style>

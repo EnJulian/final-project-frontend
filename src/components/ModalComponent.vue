8e27420f0323de3a32b9a8c35a9fe54f6d5a9063
niiodoi15 <134379645+niiodoi15@users.noreply.github.com>
2023-10-01 00:04:52 +0000

    first commit. only main modal working

---
+++ src/components/ModalComponent.vue
@@ -0,0 +1,208 @@
+<template>
+    <div class="main-container">
+        <div @click="close" class="background"></div>
+
+        <div class="application-entries">
+            <div class="profile-picture">
+                <img src="" alt="app profile picture" />
+            </div>
+            <p class="modal-text">Personal Details</p>
+            <hr />
+            <div class="main-form">
+                <div class="section-1">
+                    <label for="">Name</label>
+                    <input placeholder="Ify Chinke" value="fullName" type="text" readonly />
+
+                    <label for="">Address</label>
+                    <input placeholder="3 Sabo Ave, Yaba, Lagos" value="applicant.address" type="text" readonly />
+
+                    <label for="">Course of study</label>
+                    <input placeholder="Computer Science" value="applicant.course" type="text" readonly />
+
+                    <label for="">CGPA</label>
+                    <input placeholder="5.0" value="applicant.cgpa" type="text" readonly />
+
+                    <button @click="approve">
+                        Approve
+                    </button>
+                </div>
+
+                <div class="section-2">
+                    <label for="">Email</label>
+                    <input placeholder="ify@enyata.com" value="applicant.emailAddress" type="text" readonly />
+
+                    <label for="">University</label>
+                    <input placeholder="University of Nigeria Nsukka" value="applicant.university" type="text" readonly />
+
+                    <label for="">Date of birth</label>
+                    <input placeholder="12/09/19 - 22" value="applicantDate" type="text" readonly />
+
+                    <label for="">CV</label>
+                    <input value="applicant.cv" type="text" />
+
+                    <button @click="decline">
+                        Decline
+                    </button>
+                </div>
+            </div>
+            <div>
+                <p>Pending</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import { ref } from "vue";
+
+const denyModalVisibility = ref(false);
+const approveModalVisibility = ref(false);
+
+const openApproveModal = () => {
+    approveModalVisibility.value = true;
+}
+
+// const store = useStore();
+
+// const fullName = computed(() => {
+//     return store.getters.getApplicant.firstName + " " + store.getters.getApplicant.lastName;
+// });
+
+// const applicant = computed(() => {
+//     return store.getters.getApplicant;
+// });
+
+// const applicantDate = computed(() => {
+//     return `${props.date} - ${props.age}`;
+// });
+
+function close() {
+    emit("close");
+}
+
+function approve() {
+    emit("approve");
+}
+
+function decline() {
+    emit("decline");
+}
+</script>
+
+<style scoped>
+.main-container {
+    display: flex;
+    width: 100%;
+    height: 100%;
+}
+
+.background {
+    width: 80%;
+    height: 1050px;
+    background: rgba(3, 17, 49, 0.2);
+    backdrop-filter: blur(4px);
+}
+
+.application-entries {
+    width: 600px;
+    background: #ffffff;
+    padding: 95px 58px 145px 48px;
+}
+
+.profile-picture {
+    width: 179px;
+    height: 179px;
+    margin-bottom: 42px;
+    background-color: #7557d3;
+}
+
+.modal-text {
+    font-family: "Lato";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 19px;
+    color: #7d7d7d;
+    margin-bottom: 14px;
+}
+
+hr {
+    width: 498px;
+    height: 1px;
+    background: #f2f2f2;
+    margin-bottom: 32px;
+    border: none;
+}
+
+.main-form {
+    display: flex;
+    gap: 0 32px;
+}
+
+.main-form label {
+    font-family: "Lato";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 17px;
+    color: #b1b1b1;
+}
+
+.main-form input {
+    width: 231px;
+    height: 48px;
+    border-radius: 4px;
+    margin-bottom: 36px;
+    border: 1px solid #cdcfd6;
+    border-radius: 4px;
+}
+
+.main-form button {
+    width: 125px;
+    height: 48px;
+    font-family: "Lato";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 19px;
+    border-radius: 4px;
+}
+
+.section-1 button {
+    background: #7557d3;
+    color: #ffffff;
+    border: 1px solid #cdcfd6;
+    margin-left: 115px;
+}
+
+.section-2 button {
+    color: #4f4f4f;
+    border: 1px solid #cecece;
+    background: #ffffff;
+}
+
+button {
+    cursor: pointer;
+}
+
+input {
+    padding: 14px 18px;
+}
+
+img {
+    width: 100%;
+    height: 100%;
+}
+
+.status-approved {
+    color: #31d283;
+}
+
+.status-declined {
+    color: red;
+}
+
+.status {
+    text-align: center;
+}</style>
--- src/views/admin/ApplicationEntriesView.vue
+++ src/views/admin/ApplicationEntriesView.vue
@@ -1,5 +1,199 @@
-<script setup></script>
+<template>
+  <ModalComponent @approve="openApproveButton" @decline="openDenyButton" @close="closeModal" v-show="mainModalVisibility" class="main-modal" />
+  <DeclineModalComponent @close="closeDecisionModal" v-show="declineVisibility" class="deny-decision" />
+  <ApproveModalComponent @close="closeDecisionModal" v-show="approveVisibility" class="approve-decision" />
+  
+      <div class="main">
+        <h1>
+          Entries - Batch 1 <img src="@/assets/entries-logo.svg" alt="logo" />
+        </h1>
+        <h2>Comprises of all that applied for batch 1</h2>
 
-<template></template>
+        <table style="width: 100%">
+          <tr class="table-heading">
+            <th>Name</th>
+            <th>Email</th>
+            <th class="th">
+              <div class="sort-header">
+                <p>DOB - Age</p>
+                <figure>
+                  <img @click="ageAscending" class="sortup" src="@/assets/sortup.svg" alt="sortup" srcset="" />
 
-<style scoped></style>
+                  <img @click="ageDescending" class="sortdown" src="@/assets/sortdown.svg" alt="sortdown" srcset="" />
+                </figure>
+              </div>
+            </th>
+            <th>Address</th>
+            <th>University</th>
+            <th class="th">
+              <div>
+                <p>CGPA</p>
+                <figure>
+                  <img @click="gpaAscending" class="sortup" src="@/assets/sortup.svg" alt="sortup" />
+
+                  <img @click="gpaDescending" class="sortdown" src="@/assets/sortdown.svg" alt="sortdown" />
+                </figure>
+              </div>
+            </th>
+          </tr>
+          <tr  @click="openMainModal()"
+            class="rowss">
+            <td>
+              jACK Ice
+            </td>
+            <td>llllll</td>
+            <td>lllll</td>
+            <td>hhhhhhh</td>
+            <td>kkkkk</td>
+            <td>pppppp</td>
+          </tr>
+        </table>
+      </div>
+
+</template>
+
+<script setup>
+import { ref, computed } from 'vue';
+import ApproveModalComponent from '../../components/ApproveModalComponent.vue'
+import DeclineModalComponent from '../../components/DeclineModalComponent.vue'
+import ModalComponent from '../../components/ModalComponent.vue'
+
+
+const mainModalVisibility = ref(false);
+const declineVisibility = ref(false);
+const approveVisibility = ref(false);
+
+
+
+const openMainModal = () => {
+  mainModalVisibility.value = true;
+};
+
+const openDenyButton = () => {
+  mainModalVisibility.value = false;
+  declineVisibility.value = true;
+};
+
+const openApproveButton = () => {
+  mainModalVisibility.value = false;
+  approveVisibility.value = true;
+};
+
+const closeModal = () => {
+  mainModalVisibility.value = false;
+  // store.commit("clearApplicant");
+};
+
+const closeDecisionModal = () => {
+  approveVisibility.value = false;
+  declineVisibility.value = false;
+};
+
+
+</script>
+
+<style scoped>
+.sortup {
+  padding-bottom: 2px;
+}
+
+input {
+  border: none;
+  background-color: #2b3c4e;
+}
+
+/* .numbs {
+  right: 130px;
+} */
+h1 {
+  font-style: normal;
+  font-weight: 300;
+  font-size: 43.5555px;
+  line-height: 52px;
+  letter-spacing: -0.02em;
+  color: #2b3c4e;
+}
+
+h2 {
+  font-style: italic;
+  font-weight: 400;
+  font-size: 13px;
+  line-height: 16px;
+  margin-top: 3px;
+  margin-bottom: 38px;
+  color: #4f4f4f;
+}
+
+.table-heading {
+  width: 1042px;
+  height: 46px;
+  color: white;
+  background: #2b3c4e;
+  border-collapse: collapse;
+}
+.sort-header{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: auto;
+}
+td {
+  padding: 24px 26px;
+}
+
+table {
+  border-collapse: collapse;
+}
+
+img {
+  cursor: pointer;
+}
+
+.main {
+  margin-right: 40px;
+}
+
+.main-modal {
+  position: absolute;
+}
+
+.deny-decision {
+  position: absolute;
+}
+
+.approve-decision {
+  position: absolute;
+}
+
+figure {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 0;
+  align-self: center;
+}
+
+tr {
+  border-radius: 8px 0px 0px 8px;
+}
+
+.rowss:hover {
+  background: #ffffff;
+  box-shadow: 0px 5px 15px rgba(33, 31, 38, 0.05);
+  border-left: 7px solid #7557d3;
+  border-radius: 8px 0px 0px 8px;
+  margin-top: 20px;
+  cursor: pointer;
+}
+
+.th div {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0 5px;
+}
+
+.th p {
+  align-self: flex-end;
+  /* // justify-self: center; */
+}</style>

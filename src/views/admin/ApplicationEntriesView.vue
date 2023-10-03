73e110c4bde0f00380ebda4d3b7a99dfe427fbe4
niiodoi15 <134379645+niiodoi15@users.noreply.github.com>
2023-10-03 19:04:29 +0000

    sort by age and cgpa

--- src/views/admin/ApplicationEntriesView.vue
+++ src/views/admin/ApplicationEntriesView.vue
@@ -9,44 +9,39 @@
         </h1>
         <h2>Comprises of all that applied for batch 1</h2>
 
-        <table style="width: 100%">
-          <tr class="table-heading">
-            <th>Name</th>
-            <th>Email</th>
-            <th class="th">
-              <div class="sort-header">
-                <p>DOB - Age</p>
-                <figure>
-                  <img @click="ageAscending" class="sortup" src="@/assets/sortup.svg" alt="sortup" srcset="" />
-
-                  <img @click="ageDescending" class="sortdown" src="@/assets/sortdown.svg" alt="sortdown" srcset="" />
-                </figure>
-              </div>
-            </th>
-            <th>Address</th>
-            <th>University</th>
-            <th class="th">
-              <div>
-                <p>CGPA</p>
-                <figure>
-                  <img @click="gpaAscending" class="sortup" src="@/assets/sortup.svg" alt="sortup" />
-
-                  <img @click="gpaDescending" class="sortdown" src="@/assets/sortdown.svg" alt="sortdown" />
-                </figure>
-              </div>
-            </th>
-          </tr>
-          <tr  @click="openMainModal()"
-            class="rowss">
-            <td>
-              jACK Ice
-            </td>
-            <td>llllll</td>
-            <td>lllll</td>
-            <td>hhhhhhh</td>
-            <td>kkkkk</td>
-            <td>pppppp</td>
-          </tr>
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th class="sorting">
+                        DOB - Age
+                        <div class="icons">
+                            <button class="btn" @click="ageAscending"><img src="../../assets/icons/sortup.svg" alt="sortup" srcset=""></button>
+                            <button class="btn" @click="ageDescending"><img src="../../assets/icons/sortdown.svg" alt="sortdown" srcset=""></button>
+                        </div>
+                    </th>
+                    <th>Address</th>
+                    <th>University</th>
+                    <th class="sorting">
+                        CGPA
+                        <div class="icons">
+                            <button class="btn" @click="cgpaAscending"><img src="../../assets/icons/sortup.svg" alt="sortup" srcset=""></button>
+                            <button class="btn" @click="cgpaDescending"><img src="../../assets/icons/sortdown.svg" alt="sortdown" srcset=""></button>
+                        </div>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="t-row" v-for="person in sortedPeople" :key="person.id" @click="openMainModal">
+                    <td>{{ person.name }}</td>
+                    <td>{{ person.email }}</td>
+                    <td>{{ person.age }}</td>
+                    <td>{{ person.address }}</td>
+                    <td>{{ person.university }}</td>
+                    <td>{{ person.cgpa }}</td>
+                </tr>
+            </tbody>
         </table>
       </div>
 
@@ -58,57 +53,71 @@ import ApproveModalComponent from '../../components/ApproveModalComponent.vue'
 import DeclineModalComponent from '../../components/DeclineModalComponent.vue'
 import ModalComponent from '../../components/ModalComponent.vue'
 
-
 const mainModalVisibility = ref(false);
 const declineVisibility = ref(false);
 const approveVisibility = ref(false);
 
-
-
 const openMainModal = () => {
-  mainModalVisibility.value = true;
+    mainModalVisibility.value = true;
 };
 
 const openDenyButton = () => {
-  mainModalVisibility.value = false;
-  declineVisibility.value = true;
+    mainModalVisibility.value = false;
+    declineVisibility.value = true;
 };
 
 const openApproveButton = () => {
-  mainModalVisibility.value = false;
-  approveVisibility.value = true;
+    mainModalVisibility.value = false;
+    approveVisibility.value = true;
 };
 
 const closeModal = () => {
-  mainModalVisibility.value = false;
-  // store.commit("clearApplicant");
+    mainModalVisibility.value = false;
 };
 
 const closeDecisionModal = () => {
-  approveVisibility.value = false;
-  declineVisibility.value = false;
+    approveVisibility.value = false;
+    declineVisibility.value = false;
 };
 
+const people = ref([
+    { id: 1, name: 'jack jack', age: 12, email: 'jack@mail.com', address: '21 adom str.', university: 'University of Lagos', cgpa: 2.54 },
+    { id: 2, name: 'Bongo', age: 13, email: 'bongo@mail.com', address: '21 adom str.', university: 'University of Lagos', cgpa: 3.54 },
+    { id: 3, name: 'congo', age: 14, email: 'congo@mail.com', address: '21 adom str.', university: 'University of Lagos', cgpa: 3.24 },
+]);
 
+const sortedPeople = computed(() => {
+    return [...people.value].sort((a, b) => a.age - b.age || a.cgpa - b.cgpa);
+});
+
+const ageAscending = () => {
+    people.value.sort((a, b) => a.age - b.age || a.cgpa - b.cgpa);
+};
+const ageDescending = () => {
+    people.value.sort((a, b) => b.age - a.age || b.cgpa - a.cgpa);
+};
+
+const cgpaAscending = () => {
+    people.value.sort((a, b) => a.cgpa - b.cgpa || a.age - b.age);
+};
+const cgpaDescending = () => {
+    people.value.sort((a, b) => b.cgpa - a.cgpa || b.age - a.age);
+};
 </script>
 
+
 <style scoped>
-.sortup {
-  padding-bottom: 2px;
-}
 
 input {
   border: none;
   background-color: #2b3c4e;
 }
 
-/* .numbs {
-  right: 130px;
-} */
+
 h1 {
   font-style: normal;
   font-weight: 300;
-  font-size: 43.5555px;
+  font-size: 44px;
   line-height: 52px;
   letter-spacing: -0.02em;
   color: #2b3c4e;
@@ -124,33 +133,55 @@ h2 {
   color: #4f4f4f;
 }
 
-.table-heading {
-  width: 1042px;
-  height: 46px;
-  color: white;
-  background: #2b3c4e;
-  border-collapse: collapse;
-}
-.sort-header{
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin: auto;
-}
-td {
-  padding: 24px 26px;
-}
-
 table {
   border-collapse: collapse;
+  width: 100%;
+  text-align: justify;
 }
 
+thead{
+    background-color: #2B3C4E;
+    color: #fff;
+    padding: 8px;
+}
+th{
+    color: #fff;
+    padding: 14px 10px;
+}
+.sorting{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+.icons{
+    display: flex;
+    flex-direction: column;
+    gap: 3px;
+}
 img {
   cursor: pointer;
 }
 
+.t-row:hover {
+  border-radius: 8px;
+    background: #FFF;
+    box-shadow: 0px 5px 15px 0px rgba(33, 31, 38, 0.05);
+    border-left: 7px #7557D3 solid;
+    margin-left: 7px;
+}
+
+td{
+    padding: 22px 10px;
+    
+}
+.btn{
+    border: none;
+    background-color: transparent;
+    padding: 0;
+    margin: 0;
+}
 .main {
-  margin-right: 40px;
+    margin-right: 40px;
 }
 
 .main-modal {
@@ -165,27 +196,6 @@ img {
   position: absolute;
 }
 
-figure {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  gap: 0;
-  align-self: center;
-}
-
-tr {
-  border-radius: 8px 0px 0px 8px;
-}
-
-.rowss:hover {
-  background: #ffffff;
-  box-shadow: 0px 5px 15px rgba(33, 31, 38, 0.05);
-  border-left: 7px solid #7557d3;
-  border-radius: 8px 0px 0px 8px;
-  margin-top: 20px;
-  cursor: pointer;
-}
-
 .th div {
   display: flex;
   align-items: center;
@@ -196,4 +206,5 @@ tr {
 .th p {
   align-self: flex-end;
   /* // justify-self: center; */
-}</style>
+}
+</style>
